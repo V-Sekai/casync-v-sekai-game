f454b56c968d710c8832183b47c59bb3180d6a46
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2023-10-29 10:38:03 -0700

    Refactor extract_and_build function, add executable permission

--- extract.sh
+++ extract.sh
@@ -4,14 +4,13 @@
 #
 # The `untar` command is used to reconstruct the original data from the `.caidx` index file.
 #
-# Make sure that `desync` (which is the binary for `casync`) has executable permissions. If not, you can add them using the command `chmod +x ./thirdparty/desync_0.9.5_linux_amd64/desync`.
-#
 # - [Casync Documentation](https://github.com/systemd/casync)
 # - [Desync GitHub Repository](https://github.com/folbricht/desync)
 
 extract_and_build() {
   rm -rf $1 $1.tar
   mkdir -p $1
+  chmod +x ./$DESYNC_COMMAND
   ./$DESYNC_COMMAND untar --no-same-owner --store store -i $1.caidx $1/
 }
 #./$DESYNC_COMMAND tar -i -s ./store/ ./$1.caidx $1
